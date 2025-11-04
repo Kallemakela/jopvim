d368b6d75778ddea183ea5c073512e306e04be59
Kallemakela <kalle.makela@aalto.fi>
2025-11-04 12:00:56 +0200

    telescope jk scroll

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,6 +32,13 @@ require('telescope').setup({
             "%.egg%-info/",
             "%.lock",
         },
+        mappings = {
+            i = {
+                ["<C-j>"] = require("telescope.actions").move_selection_next,
+                ["<C-k>"] = require("telescope.actions").move_selection_previous,
+                ["<C-l>"] = require("telescope.actions").select_default,
+            },
+        },
     },
     pickers = {
         find_files = {
